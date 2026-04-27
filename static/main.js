538aa8e3ffda964d27819c8f645e74a1ecc0c544
Joseph-fash <utihfavour600@gmail.com>
2026-04-27 14:55:57 +0100

    changes

--- static/main.js
+++ static/main.js
@@ -6,20 +6,25 @@ AOS.init({
 })
 
 // Hero Swiper
-const heroSwiper = new Swiper('.heroSwiper', {
+
+const heroSwiper = new Swiper(".heroSwiper", {
   loop: true,
+  speed: 800,
+  slidesPerView: 1,
+  autoHeight: true,
   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
+
 
 
